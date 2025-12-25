b12abf57624347ecb1da2a48312a3bc379c155db
paseri538 <paseri538@gmail.com>
2025-12-26 01:04:09 +0900

    キーワード修正(バキューン)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュの名前を定義。バージョンを更新すると古いキャッシュは自動的に削除。
-const SW_VERSION = 'v20251226b'; // ★バージョンを更新
+const SW_VERSION = 'v20251226c'; // ★バージョンを更新
 const CACHE_NAME = `radio-cache-${SW_VERSION}`;
 
 
