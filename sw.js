3f7f8e15f54db2fa228b5b07f8bf215a25a6e558
paseri538 <paseri538@gmail.com>
2026-01-14 22:23:31 +0900

    95回対応

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュの名前を定義。バージョンを更新すると古いキャッシュは自動的に削除。
-const SW_VERSION = 'v20251226c'; // ★バージョンを更新
+const SW_VERSION = '20260114a'; // ★バージョンを更新
 const CACHE_NAME = `radio-cache-${SW_VERSION}`;
 
 
@@ -24,6 +24,7 @@ const CORE_ASSETS = [
 
 // 【重要】ここに、ローカルの全サムネイル画像のパス（.jpg のみ）を貼り付けます
 const THUMBNAIL_ASSETS = [
+  'thumbnails/95.jpg',
   'thumbnails/94.jpg',
   'thumbnails/93.jpg',
   'thumbnails/92.jpg',
