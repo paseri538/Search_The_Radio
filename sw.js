d737a8abdc9c28e8cb4f1f1803ceb17638c10af9
paseri538 <paseri538@gmail.com>
2026-01-26 02:10:28 +0900

    pc版でスクロールできないバグ修正

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュの名前を定義。バージョンを更新すると古いキャッシュは自動的に削除。
-const SW_VERSION = '20260114a'; // ★バージョンを更新
+const SW_VERSION = '20260126a'; // ★バージョンを更新
 const CACHE_NAME = `radio-cache-${SW_VERSION}`;
 
 
