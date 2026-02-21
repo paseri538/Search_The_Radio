c516cb85e2affaf115b0d343d915f010ff8dd372
paseri538 <paseri538@gmail.com>
2026-02-21 17:26:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュの名前を定義。バージョンを更新すると古いキャッシュは自動的に削除。
-const SW_VERSION = '20260206b'; // ★バージョンを更新
+const SW_VERSION = '20260221a'; // ★バージョンを更新
 const CACHE_NAME = `radio-cache-${SW_VERSION}`;
 
 
@@ -24,6 +24,7 @@ const CORE_ASSETS = [
 
 // ここにローカルの全サムネイル画像のパス（.jpg のみ）を貼り付けます
 const THUMBNAIL_ASSETS = [
+  'thumbnails/97.jpg',
   'thumbnails/96.jpg',
   'thumbnails/95.jpg',
   'thumbnails/94.jpg',
